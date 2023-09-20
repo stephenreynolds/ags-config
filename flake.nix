102a388f7c8287f39e26c1da8c395a3733bb77d1
Stephen Reynolds <mail@stephenreynolds.dev>
2023-09-20 15:48:25 -0400

    Revert "chore(flake): output dependencies"
    This reverts commit dd3c5e0415a30a09df141851278938d0d8c429d4.

--- flake.nix
+++ flake.nix
@@ -9,17 +9,16 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
-    rec {
+    {
       configDir = ./src;
-      buildInputs = with pkgs; [
-        ags.packages.${system}.default
-        nodejs
-        sassc
-        inotify-tools
-      ];
 
       devShells.${system}.default = pkgs.mkShell {
-        inherit buildInputs;
+        buildInputs = with pkgs; [
+          ags.packages.${system}.default
+          nodejs
+          sassc
+          inotify-tools
+        ];
       };
     };
 }
