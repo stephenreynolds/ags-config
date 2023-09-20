edd310a622c14d20823ff9ade6045c0ad9ecab98
Stephen Reynolds <mail@stephenreynolds.dev>
2023-09-20 11:21:55 -0400

    feat(bar/clock): put date and time on separate lines

--- js/bar/widgets/clock.js
+++ js/bar/widgets/clock.js
@@ -1,12 +1,29 @@
 const { execAsync } = ags.Utils;
-const { Label } = ags.Widget;
+const { Box, Label } = ags.Widget;
 
-const Clock = () => Label({
-    className: "clock",
+const Time = () => Label({
+    halign: "end",
     connections: [[
-        1000, label => execAsync(["date", "+%a %b %e %-I:%M %p"])
+        1000, label => execAsync(["date", "+%-I:%M %p"])
+            .then(time => label.label = time).catch(console.error)
+    ]]
+})
+
+const Date = () => Label({
+    halign: "end",
+    connections: [[
+        1000, label => execAsync(["date", "+%a %b %e"])
             .then(date => label.label = date).catch(console.error)
     ]]
+})
+
+const Clock = () => Box({
+    className: "clock",
+    vertical: true,
+    children: [
+        Time(),
+        Date(),
+    ],
 })
 
 export default Clock;
