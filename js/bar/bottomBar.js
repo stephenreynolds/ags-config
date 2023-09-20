8304f55b13cc6ca6d26e3c470ab07ebb6f381c49
Stephen Reynolds <mail@stephenreynolds.dev>
2023-09-20 11:21:20 -0400

    feat(bar): remove example widgets

--- js/bar/bottomBar.js
+++ js/bar/bottomBar.js
@@ -1,43 +1,38 @@
 import Workspaces from "./widgets/workspaces.js";
-import Notification from "./widgets/notification.js";
-import Volume from "./widgets/volume.js";
-import SysTray from "./widgets/systray.js";
 import Clock from "./widgets/clock.js";
 
 const { Box, CenterBox, Window } = ags.Widget;
 
 const Left = (monitor) => Box({
-  className: "start",
-  children: [
-    Workspaces(monitor),
-  ],
+    className: "start",
+    children: [
+        Workspaces(monitor),
+    ],
 });
 
 const Center = () => Box({
-  className: "center",
-  children: [
-    Notification(),
-  ],
+    className: "center",
+    children: [
+    ],
 });
 
 const Right = () => Box({
-  className: "end",
-  children: [
-    Volume(),
-    SysTray(),
-    Clock(),
-  ],
+    className: "end",
+    halign: "end",
+    children: [
+        Clock(),
+    ],
 });
 
 export default monitor => Window({
-  name: `bar-${monitor}`,
-  monitor,
-  anchor: ["bottom", "left", "right"],
-  exclusive: true,
-  child: CenterBox({
-    className: "bar",
-    startWidget: Left(monitor),
-    centerWidget: Center(),
-    endWidget: Right(),
-  }),
+    name: `bar-${monitor}`,
+    monitor,
+    anchor: ["bottom", "left", "right"],
+    exclusive: true,
+    child: CenterBox({
+        className: "bar",
+        startWidget: Left(monitor),
+        centerWidget: Center(),
+        endWidget: Right(),
+    }),
 });
