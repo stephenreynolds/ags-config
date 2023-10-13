41dadda7787868bdaf18b759712b870c90c75b55
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-13 12:19:30 -0400

    chore(flake): add flake.nix

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    ags.url = "github:Aylur/ags";
+  };
+
+  outputs = { self, nixpkgs, ags }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      configDir = ./src;
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          ags.packages.${system}.default
+          nodejs
+          sassc
+          inter
+        ];
+      };
+    };
+}
