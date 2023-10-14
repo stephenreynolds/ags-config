24f9ec7a622f2dec085eab68f27647adc2da1fe0
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-14 12:12:35 -0400

    style(verification): use double quotes

--- src/js/powermenu/Verification.js
+++ src/js/powermenu/Verification.js
@@ -1,34 +1,34 @@
-import PopupWindow from '../misc/PopupWindow.js';
-import PowerMenu from '../services/powermenu.js';
-import { Widget, App, Utils } from '../imports.js';
+import PopupWindow from "../misc/PopupWindow.js";
+import PowerMenu from "../services/powermenu.js";
+import { Widget, App, Utils } from "../imports.js";
 
 export default () => PopupWindow({
-    name: 'verification',
+    name: "verification",
     expand: true,
     content: Widget.Box({
-        className: 'verification',
+        className: "verification",
         vertical: true,
         children: [
             Widget.Label({
-                className: 'title',
-                binds: [['label', PowerMenu, 'title']],
+                className: "title",
+                binds: [["label", PowerMenu, "title"]],
             }),
             Widget.Label({
-                className: 'desc',
-                label: 'Are you sure?',
+                className: "desc",
+                label: "Are you sure?",
             }),
             Widget.Box({
-                className: 'buttons',
+                className: "buttons",
                 vexpand: true,
-                valign: 'end',
+                valign: "end",
                 homogeneous: true,
                 children: [
                     Widget.Button({
-                        child: Widget.Label('No'),
-                        onClicked: () => App.toggleWindow('verification'),
+                        child: Widget.Label("No"),
+                        onClicked: () => App.toggleWindow("verification"),
                     }),
                     Widget.Button({
-                        child: Widget.Label('Yes'),
+                        child: Widget.Label("Yes"),
                         onClicked: () => Utils.exec(PowerMenu.cmd),
                     }),
                 ],
