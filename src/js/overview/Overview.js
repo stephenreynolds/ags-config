1446b868266d287c5288cb4055fd0b85ad394bdc
Stephen Reynolds <mail@stephenreynolds.dev>
2023-10-16 21:37:48 -0400

    feat(overview): add media, network, bluetooth, and dnd controls

--- src/js/overview/Overview.js
+++ src/js/overview/Overview.js
@@ -2,6 +2,10 @@ import PopupWindow from "../misc/PopupWindow.js";
 import Applauncher from "./Applauncher.js";
 import Notifications from "./Notifications.js";
 import Calendar from "./Calendar.js";
+import Media from "./Media.js";
+import { NetworkToggle, WifiSelection } from "./Network.js";
+import { BluetoothToggle, BluetoothDevices } from "./Bluetooth.js";
+import DND from "./DND.js";
 import { Volume, SinkSelector, AppMixer } from "./Volume.js";
 import { Widget } from "../imports.js";
 
@@ -16,18 +20,34 @@ const Row = (toggles, menus = []) => Widget.Box({
     ],
 });
 
+const Homogeneous = toggles => Widget.Box({
+    homogeneous: true,
+    children: toggles,
+});
+
 const Overview = () => Widget.CenterBox({
     className: "overview",
     spacing: 20,
     startWidget: Widget.Box({
         className: "left",
+        vertical: true,
+        halign: "start",
+        spacing: 10,
         children: [
+            Media(),
             Widget.Box({
                 className: "volume slider-box",
                 valign: "start",
                 child: Row(
                     [Volume()],
                     [SinkSelector(), AppMixer()],
+                ),
+            }),
+            Widget.Box({
+                className: "network-widget",
+                child: Row(
+                    [Homogeneous([NetworkToggle(), BluetoothToggle()]), DND()],
+                    [WifiSelection(), BluetoothDevices()],
                 ),
             })
         ],
