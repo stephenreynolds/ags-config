d9b5a2c94003edde88c1ecb15fbee1b771dd87ba
Stephen Reynolds <mail@stephenreynolds.dev>
2023-11-06 18:12:16 -0500

    fix(notifications): do not show in notification list if transient

--- src/js/notifications/Notifications.js
+++ src/js/notifications/Notifications.js
@@ -5,24 +5,25 @@ const Popups = () => {
     const map = new Map();
 
     const onDismissed = (box, id, force = false) => {
-        if (!id || !map.has(id))
-            return;
+        if (!id || !map.has(id)) return;
 
-        if (map.get(id)._hovered.value && !force)
-            return;
+        if (map.get(id)._hovered.value && !force) return;
 
-        if (map.size - 1 === 0)
-            box.get_parent().revealChild = false;
+        if (map.size - 1 === 0) box.get_parent().revealChild = false;
 
         Utils.timeout(200, () => {
             map.get(id)?.destroy();
             map.delete(id);
+
+            const n = Notifications.getNotification(id);
+            if (n.hints.transient) {
+                n.close();
+            }
         });
     };
 
     const onNotified = (box, id) => {
-        if (!id || Notifications.dnd)
-            return;
+        if (!id || Notifications.dnd) return;
 
         map.delete(id);
         map.set(id, Notification(Notifications.getNotification(id)));
@@ -42,21 +43,23 @@ const Popups = () => {
     });
 };
 
-const PopupList = ({ transition = "slide_up" } = {}) => Widget.Box({
-    class_name: "notifications-popup-list",
-    css: "padding: 1px",
-    margin: 5,
-    children: [
-        Widget.Revealer({
-            transition,
-            child: Popups(),
-        }),
-    ],
-});
+const PopupList = ({ transition = "slide_up" } = {}) =>
+    Widget.Box({
+        class_name: "notifications-popup-list",
+        css: "padding: 1px",
+        margin: 5,
+        children: [
+            Widget.Revealer({
+                transition,
+                child: Popups(),
+            }),
+        ],
+    });
 
-export default monitor => Widget.Window({
-    monitor,
-    name: `notifications-${monitor}`,
-    anchor: ["top"],
-    child: PopupList(),
-});
+export default (monitor) =>
+    Widget.Window({
+        monitor,
+        name: `notifications-${monitor}`,
+        anchor: ["top"],
+        child: PopupList(),
+    });
--- src/js/overview/Notifications.js
+++ src/js/overview/Notifications.js
@@ -2,71 +2,106 @@ import icons from "../icons.js";
 import Notification from "../misc/Notification.js";
 import { Widget, Notifications } from "../imports.js";
 
-const ClearButton = () => Widget.Button({
-    on_clicked: () => Notifications.clear(),
-    binds: [["sensitive", Notifications, "notifications", n => n.length > 0]],
-    child: Widget.Box({
+const hasNotifications = (n) =>
+    n.length > 0 && !n.some((n) => n.hints.transient);
+
+const ClearButton = () =>
+    Widget.Button({
+        on_clicked: () => Notifications.clear(),
+        binds: [
+            [
+                "sensitive",
+                Notifications,
+                "notifications",
+                (n) => hasNotifications(n),
+            ],
+        ],
+        child: Widget.Box({
+            children: [
+                Widget.Label("Clear "),
+                Widget.Icon({
+                    binds: [
+                        [
+                            "icon",
+                            Notifications,
+                            "notifications",
+                            (n) =>
+                                n.length > 0
+                                    ? icons.trash.full
+                                    : icons.trash.empty,
+                        ],
+                    ],
+                }),
+            ],
+        }),
+    });
+
+const Header = () =>
+    Widget.Box({
+        class_name: "header",
         children: [
-            Widget.Label("Clear "),
-            Widget.Icon({
-                binds: [["icon", Notifications, "notifications", n =>
-                    n.length > 0 ? icons.trash.full : icons.trash.empty]],
+            Widget.Label({ label: "Notifications", hexpand: true, xalign: 0 }),
+            ClearButton(),
+        ],
+    });
+
+const NotificationList = () =>
+    Widget.Box({
+        vertical: true,
+        vexpand: true,
+        connections: [
+            [
+                Notifications,
+                (box) => {
+                    box.children = Notifications.notifications
+                        .filter((n) => !n.hints.transient)
+                        .reverse()
+                        .map(Notification);
+
+                    box.visible = hasNotifications(Notifications.notifications);
+                },
+            ],
+        ],
+    });
+
+const Placeholder = () =>
+    Widget.Box({
+        class_name: "placeholder",
+        vertical: true,
+        vpack: "center",
+        hpack: "center",
+        vexpand: true,
+        hexpand: true,
+        children: [
+            Widget.Icon(icons.notifications.silent),
+            Widget.Label("Your inbox is empty"),
+        ],
+        binds: [
+            [
+                "visible",
+                Notifications,
+                "notifications",
+                (n) => n.length === 0 || n.every((n) => n.hints.transient),
+            ],
+        ],
+    });
+
+export default () =>
+    Widget.Box({
+        class_name: "notifications",
+        vertical: true,
+        children: [
+            Header(),
+            Widget.Scrollable({
+                vexpand: true,
+                class_name: "notification-scrollable",
+                hscroll: "never",
+                vscroll: "automatic",
+                child: Widget.Box({
+                    class_name: "notification-list",
+                    vertical: true,
+                    children: [NotificationList(), Placeholder()],
+                }),
             }),
         ],
-    }),
-});
-
-const Header = () => Widget.Box({
-    class_name: "header",
-    children: [
-        Widget.Label({ label: "Notifications", hexpand: true, xalign: 0 }),
-        ClearButton(),
-    ],
-});
-
-const NotificationList = () => Widget.Box({
-    vertical: true,
-    vexpand: true,
-    connections: [[Notifications, box => {
-        box.children = Notifications.notifications
-            .reverse().map(Notification);
-
-        box.visible = Notifications.notifications.length > 0;
-    }]],
-});
-
-const Placeholder = () => Widget.Box({
-    class_name: "placeholder",
-    vertical: true,
-    vpack: "center",
-    hpack: "center",
-    vexpand: true,
-    hexpand: true,
-    children: [
-        Widget.Icon(icons.notifications.silent),
-        Widget.Label("Your inbox is empty"),
-    ],
-    binds: [["visible", Notifications, "notifications", n => n.length === 0]],
-});
-
-export default () => Widget.Box({
-    class_name: "notifications",
-    vertical: true,
-    children: [
-        Header(),
-        Widget.Scrollable({
-            vexpand: true,
-            class_name: "notification-scrollable",
-            hscroll: "never",
-            vscroll: "automatic",
-            child: Widget.Box({
-                class_name: "notification-list",
-                vertical: true,
-                children: [
-                    NotificationList(),
-                    Placeholder(),
-                ],
-            }),
-        }),
-    ],
-});
+    });
