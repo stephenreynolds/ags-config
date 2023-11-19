cc155d8ebc69fdf83b9765088d8bbb2a4829e637
Stephen Reynolds <mail@stephenreynolds.dev>
2023-11-19 10:14:35 -0500

    feat(applauncher): fuzzy match apps

--- src/js/overview/Applauncher.js
+++ src/js/overview/Applauncher.js
@@ -2,42 +2,79 @@ import { Widget, App, Applications } from "../imports.js";
 import Separator from "../misc/Separator.js";
 import icons from "../icons.js";
 import { launchApp } from "../utils.js";
+import Fuse from "../lib/fuse.js";
 
-const AppItem = app => Widget.Button({
-    class_name: "app",
-    on_clicked: () => {
-        App.closeWindow("overview");
-        launchApp(app);
+const AppItem = (app) =>
+    Widget.Button({
+        class_name: "app",
+        on_clicked: () => {
+            App.closeWindow("overview");
+            launchApp(app);
+        },
+        child: Widget.Box({
+            children: [
+                Widget.Icon({
+                    icon: app.iconName,
+                    size: 48,
+                }),
+                Widget.Box({
+                    vertical: true,
+                    children: [
+                        Widget.Label({
+                            class_name: "title",
+                            label: app.name,
+                            xalign: 0,
+                            vpack: "center",
+                            ellipsize: 3,
+                        }),
+                        Widget.Label({
+                            class_name: "description",
+                            label: app.description || "",
+                            wrap: true,
+                            xalign: 0,
+                            justification: "left",
+                            vpack: "center",
+                        }),
+                    ],
+                }),
+            ],
+        }),
+    });
+
+const fuseOptions = {
+    keys: [
+        { name: "name", weight: 5 },
+        { name: "description", weight: 1 },
+    ],
+    includeScore: true,
+    sortFn: (a, b) => {
+        if (a.score === b.score) {
+            const aFrequency = Applications.list.find(
+                (app) =>
+                    app.name === a.item[0].v &&
+                    (a.item[1] ? app.description === a.item[1].v : true),
+            ).frequency;
+            const bFrequency = Applications.list.find(
+                (app) =>
+                    app.name === b.item[0].v &&
+                    (b.item[1] ? app.description === b.item[1].v : true),
+            ).frequency;
+
+            return bFrequency - aFrequency;
+        }
+
+        return a.score - b.score;
     },
-    child: Widget.Box({
-        children: [
-            Widget.Icon({
-                icon: app.iconName,
-                size: 48,
-            }),
-            Widget.Box({
-                vertical: true,
-                children: [
-                    Widget.Label({
-                        class_name: "title",
-                        label: app.name,
-                        xalign: 0,
-                        vpack: "center",
-                        ellipsize: 3,
-                    }),
-                    Widget.Label({
-                        class_name: "description",
-                        label: app.description || "",
-                        wrap: true,
-                        xalign: 0,
-                        justification: "left",
-                        vpack: "center",
-                    }),
-                ],
-            }),
-        ],
-    }),
-});
+};
+
+const fuse = new Fuse(Applications.list, fuseOptions);
+
+function filterApps(term) {
+    const entries = fuse.search(term);
+    return entries.map((entry) =>
+        Applications.list.find((app) => app.name === entry.item.name),
+    );
+}
 
 export default () => {
     const list = Widget.Box({ vertical: true });
@@ -59,10 +96,15 @@ export default () => {
             }
         },
         onChange: ({ text }) => {
-            list.children = Applications.query(text).map(app => [
-                Separator(),
-                AppItem(app),
-            ]).flat();
+            let items = [];
+            if (text === "") {
+                items = Applications.query(text);
+            } else {
+                items = filterApps(text);
+            }
+            list.children = items
+                .map((app) => [Separator(), AppItem(app)])
+                .flat();
             list.add(Separator());
             list.show_all();
 
@@ -77,10 +119,7 @@ export default () => {
         children: [
             Widget.Box({
                 class_name: "header",
-                children: [
-                    Widget.Icon(icons.apps.search),
-                    entry,
-                ],
+                children: [Widget.Icon(icons.apps.search), entry],
             }),
             Widget.Scrollable({
                 hscroll: "never",
@@ -92,13 +131,16 @@ export default () => {
                 }),
             }),
         ],
-        connections: [[App, (_, name, visible) => {
-            if (name !== "overview")
-                return;
+        connections: [
+            [
+                App,
+                (_, name, visible) => {
+                    if (name !== "overview") return;
 
-            entry.set_text("");
-            if (visible)
-                entry.grab_focus();
-        }]],
+                    entry.set_text("");
+                    if (visible) entry.grab_focus();
+                },
+            ],
+        ],
     });
 };
