bb3062780960f1f9cb5d551af615e2419d961a48
Stephen Reynolds <mail@stephenreynolds.dev>
2023-12-11 15:01:06 -0500

    feat(bar): add system indicators for volume and network

--- src/js/bar/Bar.js
+++ src/js/bar/Bar.js
@@ -2,6 +2,7 @@ import Widget from "resource:///com/github/Aylur/ags/widget.js";
 import Workspaces from "./Workspaces.js";
 import OverviewButton from "./OverviewButton.js";
 import SystemTray from "./SystemTray.js";
+import SystemIndicators from "./SystemIndicators.js";
 import options from "../options.js";
 
 /** @param {number} monitor */
@@ -11,8 +12,10 @@ const Bar = (monitor) => Widget.CenterBox({
     centerWidget: OverviewButton(monitor),
     endWidget: Widget.Box({
         hpack: "end",
+        spacing: 20,
         children: [
-            monitor === options.primaryMonitor ? SystemTray() : null
+            monitor === options.primaryMonitor ? SystemTray() : null,
+            SystemIndicators(),
         ],
     }),
 });
