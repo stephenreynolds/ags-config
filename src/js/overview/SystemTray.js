65d6b310158a2f9fef34dfab0697de08bc7f3f3d
Stephen Reynolds <mail@stephenreynolds.dev>
2023-12-12 00:04:02 -0500

    fix(systemtray): fix systemtray

--- src/js/overview/SystemTray.js
+++ src/js/overview/SystemTray.js
@@ -4,27 +4,30 @@ import SystemTray from "resource:///com/github/Aylur/ags/service/systemtray.js";
 import Gdk from "gi://Gdk";
 
 const onTrayItemClick = (item, btn) =>
-    item.menu.popup_at_widget(btn, Gdk.Gravity.SOUTH, Gdk.Gravity.NORTH, null);
+    item.menu?.popup_at_widget(btn, Gdk.Gravity.NORTH, Gdk.Gravity.SOUTH, null);
 
 const SysTrayItem = (item) =>
     PanelButton({
         content: Widget.Icon({ binds: [["icon", item, "icon"]] }),
         binds: [["tooltipMarkup", item, "tooltip-markup"]],
         setup: (btn) => {
-            const id = item.menu.connect("popped-up", (menu) => {
+            const id = item.menu?.connect("popped-up", (menu) => {
                 btn.toggleClassName("active");
                 menu.connect("notify::visible", (menu) => {
                     btn.toggleClassName("active", menu.visible);
                 });
                 menu.disconnect(id);
             });
+
+            if (id) {
+                btn.connect("destroy", () => item.menu?.disconnect(id));
+            }
         },
         onPrimaryClick: (btn) => onTrayItemClick(item, btn),
         onSecondaryClick: (btn) => onTrayItemClick(item, btn),
     });
 
-export default () =>
-    Widget.Box({
-        class_name: "system-tray",
-        binds: [["children", SystemTray, "items", (i) => i.map(SysTrayItem)]],
-    });
+export default () => Widget.Box({
+    class_name: "system-tray",
+    binds: [["children", SystemTray, "items", (i) => i.map(SysTrayItem)]],
+});
