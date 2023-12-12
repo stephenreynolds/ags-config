28284cb43d941502dd0ec1f083aea98c84e7b773
Stephen Reynolds <mail@stephenreynolds.dev>
2023-12-11 22:27:43 -0500

    refactor: remove unused options and variables

--- src/js/options.js
+++ src/js/options.js
@@ -1,9 +1,3 @@
 export default {
-    // if this player is running this will be shown on panel
-    preferredMpris: "spotify",
-
     primaryMonitor: 2,
-
-    // at what intervals should cpu, ram, temperature refresh
-    systemFetchInterval: 5000,
 };
