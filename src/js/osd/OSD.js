39913e2fb6f9973ff9497b2190de57682c24a8a5
Stephen Reynolds <mail@stephenreynolds.dev>
2023-12-11 20:04:49 -0500

    feat(osd): improve volume indicator

--- src/js/osd/OSD.js
+++ src/js/osd/OSD.js
@@ -1,46 +1,66 @@
 import Widget from "resource:///com/github/Aylur/ags/widget.js";
 import Audio from "resource:///com/github/Aylur/ags/service/audio.js";
 import Indicator from "../services/onScreenIndicator.js";
+import icons from "../icons.js";
 
-const OsdValue = (name, labelConnections, progressConnections, props = {}) => Widget.Box({ // Volume
-    ...props,
+const VolumeIndicator = Widget.Box({
     vertical: true,
     className: "osd-bg osd-value",
     hexpand: true,
+    spacing: 5,
     children: [
         Widget.Box({
-            vexpand: true,
+            spacing: 5,
             children: [
-                Widget.Label({
-                    xalign: 0, yalign: 0, hexpand: true,
-                    className: "osd-label",
-                    label: `${name}`,
+                Widget.Slider({
+                    className: "osd-volume-slider",
+                    hexpand: true,
+                    vertical: false,
+                    drawValue: false,
+                    onChange: ({ value }) => Audio.speaker.volume = value,
+                    connections: [[Audio, (progress) => {
+                        const updateValue = Audio.speaker?.volume;
+                        if (!isNaN(updateValue)) progress.value = updateValue;
+                    }]],
                 }),
-                Widget.Label({
-                    hexpand: false, className: "osd-value-txt",
-                    label: "100",
-                    connections: labelConnections,
+                Widget.Box({
+                    vexpand: true,
+                    spacing: 5,
+                    children: [
+                        Widget.Label({
+                            hexpand: false, className: "osd-value-txt",
+                            label: "100",
+                            connections: [[Audio, (label) => {
+                                label.label = `${Math.round(Audio.speaker?.volume * 100)}`;
+                            }]],
+                        }),
+                        Widget.Icon({
+                            connections: [[Audio, (icon) => {
+                                if (!Audio.speaker)
+                                    return;
+
+                                const { muted, low, medium, high, overamplified } = icons.audio.volume;
+                                if (Audio.speaker.stream.isMuted)
+                                    return icon.icon = muted;
+
+                                /** @type {Array<[number, string]>} */
+                                const cons = [[101, overamplified], [67, high], [34, medium], [0, low]];
+                                icon.icon = cons.find(([n]) => n <= Audio.speaker.volume * 100)?.[1] || "";
+                            }, "speaker-changed"]],
+                        })
+                    ]
                 }),
             ]
         }),
-        Widget.ProgressBar({
-            className: "osd-progress",
-            hexpand: true,
-            vertical: false,
-            connections: progressConnections,
-        })
+        Widget.Label({
+            className: "text-sm",
+            truncate: "end",
+            connections: [[Audio, (label) => {
+                label.label = Audio.speaker?.description || "";
+            }]]
+        }),
     ],
 });
-
-const VolumeIndicator = OsdValue("Volume",
-    [[Audio, (label) => {
-        label.label = `${Math.round(Audio.speaker?.volume * 100)}`;
-    }]],
-    [[Audio, (progress) => {
-        const updateValue = Audio.speaker?.volume;
-        if (!isNaN(updateValue)) progress.value = updateValue;
-    }]],
-);
 
 export default (monitor) => Widget.Window({
     name: `indicator-${monitor}`,
