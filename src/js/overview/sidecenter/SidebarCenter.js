c708f308623746c99437f48874b438f956caaff5
Stephen Reynolds <mail@stephenreynolds.dev>
2023-12-13 14:19:47 -0500

    feat(system-tray): add system tray

--- src/js/overview/sidecenter/SidebarCenter.js
+++ src/js/overview/sidecenter/SidebarCenter.js
@@ -1,6 +1,7 @@
 import App from 'resource:///com/github/Aylur/ags/app.js';
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
 import AppLauncher from './AppLauncher.js';
+import SystemTray from './SystemTray.js';
 
 export default () => Widget.Box({
     className: 'sidebar-center sidecenter-show sidecenter-hide',
@@ -8,7 +9,17 @@ export default () => Widget.Box({
     vexpand: true,
     hpack: 'center',
     children: [
-        AppLauncher(),
+        Widget.Box({
+            className: 'sidecenter-group',
+            vpack: 'center',
+            child: AppLauncher(),
+        }),
+        Widget.Box({
+            className: 'sidecenter-group',
+            vpack: 'end',
+            hpack: 'center',
+            child: SystemTray(),
+        }),
     ],
     connections: [[
         App, (self, currentName, visible) => {
