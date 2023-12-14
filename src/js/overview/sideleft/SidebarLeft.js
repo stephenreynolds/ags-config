2fa125ea1cc938a3f6ef8b0bca96c6e4bd39d467
Stephen Reynolds <mail@stephenreynolds.dev>
2023-12-14 02:28:43 -0500

    feat(overview): add volume, audio device, and app mixer controls

--- src/js/overview/sideleft/SidebarLeft.js
+++ src/js/overview/sideleft/SidebarLeft.js
@@ -1,11 +1,17 @@
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
+import Volume from './Volume.js';
 
 export default () => Widget.Box({
     class_name: 'sidebar-left',
     vertical: true,
     vexpand: true,
     hpack: 'start',
-    children: [
-        Widget.Label({ label: 'test' }),
-    ],
+    child: Widget.Box({
+        vertical: true,
+        vexpand: true,
+        className: 'spacing-v-15',
+        children: [
+            Volume(),
+        ],
+    }),
 });
